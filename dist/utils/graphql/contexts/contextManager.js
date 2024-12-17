e357c11c9cc59ce0045c759233dd70ffc1327488
Spencer Kotowick <1714316+kotowick@users.noreply.github.com>
2024-12-17 14:03:13 -0800

    See if stack exists

--- dist/utils/graphql/contexts/contextManager.js
+++ dist/utils/graphql/contexts/contextManager.js
@@ -162,22 +162,24 @@ class ContextManager extends graphQLManager_1.default {
     `;
         const labels = [
             ...(Array.isArray(inputs.labels) ? inputs.labels.map((label) => label) : []),
-            autoAttachLabel
+            autoAttachLabel,
         ];
         const variables = {
             input: {
-                name: inputs.name, // Required
-                description: inputs.description || '', // Optional
-                space: inputs.space || null, // Optional
-                labels: labels, // Required
-                configAttachments: inputs.configAttachments.map((config) => ({
-                    id: config.id, // Must be provided
-                    type: config.type || 'ENVIRONMENT_VARIABLE', // Default to 'ENVIRONMENT_VARIABLE'
-                    value: Array.isArray(config.value) ? JSON.stringify(config.value) : config.value || '', // Stringify arrays
-                    writeOnly: config.writeOnly !== undefined ? config.writeOnly : true, // Default to 'true'
-                    description: config.description || '', // Optional
-                    fileMode: config.fileMode || '0644', // Optional, provide a default if needed
-                })) || [], // Required
+                name: inputs.name,
+                description: inputs.description || '',
+                space: inputs.space || null,
+                labels: labels,
+                configAttachments: Array.isArray(inputs.configAttachments)
+                    ? inputs.configAttachments.map((config) => ({
+                        id: config.id,
+                        type: config.type || 'ENVIRONMENT_VARIABLE',
+                        value: Array.isArray(config.value) ? JSON.stringify(config.value) : config.value || '',
+                        writeOnly: config.writeOnly !== undefined ? config.writeOnly : true,
+                        description: config.description || '',
+                        fileMode: config.fileMode || '0644',
+                    }))
+                    : [],
                 hooks: {
                     beforeInit: inputs.hooks?.beforeInit || [],
                     afterInit: inputs.hooks?.afterInit || [],
@@ -191,7 +193,7 @@ class ContextManager extends graphQLManager_1.default {
                     afterPerform: inputs.hooks?.afterPerform || [],
                     afterRun: inputs.hooks?.afterRun || [],
                 },
-                stackAttachments: inputs.stackAttachments || [], // Optional
+                stackAttachments: inputs.stackAttachments || [],
             },
         };
         // If updating, add ID and replaceConfigElements
