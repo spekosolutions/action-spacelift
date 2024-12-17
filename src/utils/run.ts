6e0e3e6d0267b65cb246facdc3e84392264be8b8
Spencer Kotowick <1714316+kotowick@users.noreply.github.com>
2024-12-17 12:56:28 -0800

    See if stack exists

--- src/utils/run.ts
+++ src/utils/run.ts
@@ -31,15 +31,19 @@ export const run = async (inputs: Inputs): Promise<void> => {
     const stackName = `${label_postfix}-${service_name}-${env}-${region}`
     core.info(`Using stack name: ${stackName}`)
 
-    graphqlStackManager.waitForStackRunsToFinish(stackName);
-    graphqlStackManager.waitForStackToBeReady(stackName);
-
     // Generate a unique tag
     const uniqueTag = generateUniqueTag()
     core.info(`Generated unique tag: ${uniqueTag}`)
 
     // Check if stack exists
     const existingStack = await graphqlStackManager.getStackByName(stackName)
+
+    if (existingStack) {
+      graphqlStackManager.waitForStackRunsToFinish(stackName);
+      graphqlStackManager.waitForStackToBeReady(stackName);
+    } else {
+      core.info(`Stack "${stackName}" does not exist. Proceeding to create a new stack.`);
+    }
 
     if (!existingStack || command.includes('deploy')) {
       // Declare the spaceId variable to be used later
