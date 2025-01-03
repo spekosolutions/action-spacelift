5e30d3a4e8c6528e62a2139d3b0556bc74e23a5d
Spencer Kotowick <1714316+kotowick@users.noreply.github.com>
2025-01-03 15:56:48 -0800

    allow custom deplyoment path

--- dist/index.js
+++ dist/index.js
@@ -541,7 +541,7 @@ class Config {
         if (!this.parentSpaceId) {
             throw new Error('Parent Space ID is not set. Ensure manageSpace has been executed.');
         }
-        return `-var 'parent_space_id=${this.parentSpaceId}' -var 'application=${this.serviceName}' -var 'env=${this.env}' -var 'zone=${this.zone}' -var 'region=${this.region}' -var 'env_context=${this.envContext}'`;
+        return `-var 'parent_space_id=${this.parentSpaceId}' -var 'env=${this.env}' -var 'zone=${this.zone}' -var 'region=${this.region}' -var 'env_context=${this.envContext}'`;
     }
 }
 exports["default"] = Config;
